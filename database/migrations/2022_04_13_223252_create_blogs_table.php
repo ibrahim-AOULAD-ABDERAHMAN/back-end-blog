19e8d27f2b5ad3ebaee9c3d02c7e818cd07c7b25
ibrahim-AOULAD-ABDERAHMAN <ibrahimocth@gmail.com>
2022-04-13 22:48:57 +0000

    second commit

---
+++ database/migrations/2022_04_13_223252_create_blogs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50)->nullable();
+            $table->text('body')->nullable();
+            $table->string('image', 50)->nullable();
+            $table->integer('id_category');
+            $table->foreign('id_category')->references('id')->on('categories');
+                // ->onUpdate('cascade');
+                // ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
